2a284c03407ec60cdd9fbd6e692b59ece57f7c30
NL <elias.nimlandlind@elev.ga.lbs.se>
2023-10-05 14:26:48 +0200

    The core of the transaction class seems to be working.

--- Transaction.cs
+++ Transaction.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace WebShop3
 {
-    internal class Transaction
+    public class Transaction
     {
+        public readonly List<string> BoughtProducts;
+        public readonly int AmountOfSpentMoney = 0;
 
+        public readonly DateTime TimeOfPurchase;
+
+        public Transaction(List<string> boughtProducts, int amountOfMoneySpent)
+        {
+            BoughtProducts = boughtProducts;
+            AmountOfSpentMoney = amountOfMoneySpent;
+
+
+            TimeOfPurchase = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string boughtProductformated = "Bought products: \n";
+            foreach (string boughtProduct in BoughtProducts)
+            {
+                boughtProductformated += boughtProduct + "\n";
+            }
+
+            string transactionData = boughtProductformated +
+                                                    "\n" +
+                                                    "The total sum is: "+
+                                                    AmountOfSpentMoney +
+                                                    " kr." +
+                                                    "\n\nTime of purchase: " +
+                                                    TimeOfPurchase +
+                                                    ".";
+
+            return transactionData;
+        }
     }
 }
