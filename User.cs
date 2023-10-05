f18d3a0d29ca0b56639af83c4f0da3b91df74e8f
Arkainas <kevin.vikander@gmail.com>
2023-10-05 13:39:41 +0200

    added a constructor for password and login email

---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop3;
+
+public class User
+{
+    string email;
+    string passWord;
+    public User(string loginEmail, string loginPassWord)
+    {
+        email = loginEmail;
+        passWord = loginPassWord;
+    }
+}
+
