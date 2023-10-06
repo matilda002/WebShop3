29cbdc37529db6e104819057d18478d9e63227f0
NL <elias.nimlandlind@elev.ga.lbs.se>
2023-10-06 10:47:31 +0200

    .

---
+++ Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebShop3
+{    
+    /// <summary>
+     ///     A record type is used to indicate that this is used to store data, primarily.  
+     /// </summary>
+    public record Product (string Name, int Price)
+    {
+
+    }
+}
