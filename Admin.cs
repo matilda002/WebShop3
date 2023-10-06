5d87762d2385f01ad840a9bb0b32090ca98b022e
matilda002 <matilda0817@hotmail.com>
2023-10-06 10:20:59 +0200

    initial commit
    made a dictonary with the items in stock and their price

--- Admin.cs
+++ Admin.cs
@@ -2,7 +2,7 @@
 
 namespace WebShop3;
 
-public class Admin : TempoaryUser
+public class Admin : TempoaryUser, IStockList
 {
 
     public Admin (string username, string email) : base(username, email)
@@ -10,21 +10,4 @@ public class Admin : TempoaryUser
 
     }
 
-    // list for the items and their price
-    public static void Stockist()
-    {
-        Dictionary<string, int> stockPrice = new Dictionary<string, int>();
-
-        stockPrice.Add("apple", 12);
-        stockPrice.Add("orange", 20);
-        stockPrice.Add("pear", 6);
-        stockPrice.Add("plum", 70);
-        stockPrice.Add("banana", 50);
-
-        foreach (var item in stockPrice)
-        {
-            Console.WriteLine(item);
-        }
-    }
-    
 }
--- StockList.cs
+++ StockList.cs
@@ -1,9 +1,39 @@
 ﻿namespace WebShop3;
 
-public class StockList
+public interface IStockList
 {
-    private Dictionary<string, int> stockPrice = new Dictionary<string, int>();
+    public static void Stock()
+    {
+        // List for stock and price
+        List<string> stock = new List<string>();
+        List<int> price = new List<int>();
+
+        // Dictonary for items in stock with their price
+        Dictionary<string, int> stockPrice = new Dictionary<string, int>();
+
+        // Admin adding items to the stock list
+        Console.Write("Add item to list: ");
+        stock.Add(Console.ReadLine());
+
+        // Admin setting a price for the corresponding item
+        Console.Write("Set a price for the item: ");
+        price.Add(Convert.ToInt32(Console.ReadLine()));
 
 
+        // adding the item + price in the dictonary
+        for (int i = 0; i < stock.Count; i++)
+        {
+            for (int j = 0; j < price.Count; j++)
+            {
+                stockPrice.Add(stock[i], price[j]);
+            }
+        }
 
+        // printing out the dictonary containing item + price
+        foreach (var item in stockPrice)
+        {
+            Console.WriteLine(item);
+
+        }
+    }
 }
