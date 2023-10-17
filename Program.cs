250fb401dc0fa5d831028e3b890701569b95a3d1
SalarJun <salar.jundi97@gmail.com>
2023-10-17 14:33:23 +0200

    Ändrat namnet på LogInProgram till program.cs
    har påbörjat metoder för admin i Admin klassen.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using WebShop3;
+
+if (!File.Exists("products.csv"))
+{
+    File.Create("products.csv").Close();
+}
+
+ILoginSystem loginSystem = new LogIn();
+
+Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+
+Console.WriteLine("Enter password: ");
+string password = Console.ReadLine();
+
+if (loginSystem.Register(username, password))
+{
+    Console.WriteLine("You are now a registered user");
+}
+
