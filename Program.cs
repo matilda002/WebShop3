cbefc07ebdaa02438c69033bef26d900754c6400
Arkainas <kevin.vikander@gmail.com>
2023-10-19 15:32:31 +0200

    added comments, final touch ups on login

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using WebShop3;
 
 if (!File.Exists("products.csv"))
@@ -17,10 +18,33 @@ string password = Console.ReadLine();
 
 if (loginSystem.Login(username, password))
 {
-    //enter everything that the user can do
+    UserRole userRole = GetRole(username); //reads the role from the method
+
+    if (userRole == UserRole.Admin)
+    {
+        // admin specific actions
+    }
+    else
+    {
+        //user specific actions   
+    }
 }
 
-else if (loginSystem.Register(username, password, UserRole.User)) //calls on the register method to register a new user
+static UserRole GetRole(string username)
 {
-    Console.WriteLine("You are now a registered user");
+    string userFilePath = $"../../../Users/{username}.txt"; // finds directory of user files
+
+    if (File.Exists(userFilePath))
+    {
+        string[] lines = File.ReadAllLines(userFilePath); //reads all lines in the user .txt file
+        if (lines.Length >= 3)
+        {
+            string roleString = lines[2]; //saves the third row, as that's where the role is saved in the .txt file
+            if (Enum.TryParse(roleString, out UserRole role)) // parse string into the corresponding enum value
+            {
+                return role;
+            }
+        }
+    }
+    return UserRole.User; // default to the user file if it doesn't exist or is incorrect
 }
