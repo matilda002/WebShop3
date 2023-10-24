60091384e73257e03368c657a433d4f3312936cb
matilda002 <matilda0817@hotmail.com>
2023-10-24 12:00:19 +0200

    menu added for user and admin

--- Program.cs
+++ Program.cs
@@ -1,32 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using WebShop3;
 
-if (!File.Exists("products.csv"))
+// creates the file for products
+if (!File.Exists("products.csv")) // ändra till "../../../products.csv"
 {
     File.Create("products.csv").Close();
 }
 
 ILoginSystem loginSystem = new LogIn();
 
-Console.WriteLine("Enter username: ");
-string username = Console.ReadLine().ToLower();
+string? username = string.Empty;
+while (username?.Length == 0) // making sure input isn't empty
+{
+    Console.Clear();
+    Console.WriteLine("Enter username: ");
+    username = Console.ReadLine()?.ToLower();
+}
 
-Console.WriteLine("Enter password: ");
-string password = Console.ReadLine();
+string? password = string.Empty;
+while (password?.Length == 0) // making sure input isn't empty
+{
+    Console.Clear();
+    Console.WriteLine("Enter password: ");
+    password = Console.ReadLine();
+}
+
 
 if (loginSystem.Login(username, password))
 {
-    UserRole userRole = GetRole(username); //reads the role from the method
+    UserRole userRole = GetRole(username); // reads the role from the method
 
     if (userRole == UserRole.Admin)
     {
-        // admin specific actions
+        AdminMenu();
     }
     else
     {
-        //user specific actions   
+        UserMenu();   
     }
 }
 
@@ -36,10 +45,10 @@ static UserRole GetRole(string username)
 
     if (File.Exists(userFilePath))
     {
-        string[] lines = File.ReadAllLines(userFilePath); //reads all lines in the user .txt file
+        string[] lines = File.ReadAllLines(userFilePath); // reads all lines in the user .txt file
         if (lines.Length >= 3)
         {
-            string roleString = lines[2]; //saves the third row, as that's where the role is saved in the .txt file
+            string roleString = lines[2]; // saves the third row, as that's where the role is saved in the .txt file
             if (Enum.TryParse(roleString, out UserRole role)) // parse string into the corresponding enum value
             {
                 return role;
@@ -47,4 +56,77 @@ static UserRole GetRole(string username)
         }
     }
     return UserRole.User; // default to the user file if it doesn't exist or is incorrect
+}
+
+void AdminMenu()
+{
+    bool validChoice = false;
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("------- Admin Menu -------\n"+
+                          "1. Transaction overview\n" +
+                          "2. Product overview\n"+
+                          "3. Edit user" +
+                          "\n4. Quit");
+        Console.Write("\nWrite your menuchoice:  ");
+        int.TryParse(Console.ReadLine(), out int successful); // make sure the input is a number and included in the list
+
+        switch (successful) // transfered to 
+        {
+            case 1:
+                TransactionOverview to = new TransactionOverview();
+                to.UserTransaction();
+                validChoice = true; break;
+            case 2:
+                ProductMenu pm = new ProductMenu();
+                pm.StockMenu();
+                validChoice = true; break;
+            case 3:
+                EditUser eu = new EditUser();
+                eu.EditUserMenu();
+                validChoice = true; break;
+            case 4:
+                Console.WriteLine("You have chosen to quit!\n\nPress 'ENTER' to continue!"); // quit program
+                Console.ReadKey();
+                return;
+            default:
+                Console.WriteLine("Not a valid menuchoice!\n\nPress 'ENTER' to continue!");
+                Console.ReadKey();
+                break;
+        }
+    } while (!validChoice);
+}
+
+void UserMenu()
+{
+    bool validChoice = false;
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("------- Costumer Menu -------\n" +
+                          "1. Store\n" +
+                          "2. See your orderhistory" +
+                          "\n3. Quit");
+        Console.Write("\nWrite your menuchoice:  ");
+        int.TryParse(Console.ReadLine(), out int successful); // make sure the input is a number and included in the list
+
+        switch (successful)
+        {
+            case 1:
+                Store store = new Store();
+                validChoice = true; break;
+            case 2:
+                // user.DisplayTransaction
+                validChoice = true; break;
+            case 3:
+                Console.WriteLine("You have chosen to quit!\n\nPress 'ENTER' to continue!"); // quit program
+                Console.ReadKey();
+                return;
+            default:
+                Console.WriteLine("Not a valid menuchoice!\n\nPress 'ENTER' to continue!");
+                Console.ReadKey();
+                break;
+        }
+    } while (!validChoice);
 }
