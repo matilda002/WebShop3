3d18adf435594f857c54b349b6829be9f5b1280f
Elias Nimland Lind <elias.nimlandlind@elev.ga.lbs.se>
2023-10-24 11:54:53 +0200

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,17 +1,53 @@
-﻿namespace WebShop3;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace WebShop3;
 
 public class User
 {
     public string UserName { get; set; }
     public string PassWord { get; set; }
-    public UserRole Role { get; set; }
-    public User(string userName, string passWord, UserRole role = UserRole.User)
-    {
 
+    private string filePath;
+
+    List<Product> _boughtProducts = new List<Product>();
+
+    public User(string userName, string passWord)
+    {
         UserName = userName;
         PassWord = passWord;
-        Role = role;
 
+        Transaction _transaction = new Transaction(_boughtProducts);
+        filePath = $"../../../transactions/transaction_{UserName}";
+
+        SaveTransactionData(_transaction);
+        DisplayTransactionData();
+    }
+
+    public void DisplayTransactionData()
+    {
+        string[] contentOfFile = File.ReadAllLines(filePath);
+
+        foreach (string line in contentOfFile)
+        {
+            string[] info = line.Split(", ");
+
+            Product product = new Product(contentOfFile[0], int.Parse(contentOfFile[1]));
+            _boughtProducts.Add(product);
+            Transaction transaction = new Transaction(_boughtProducts);
+            Console.WriteLine(transaction.ToString());
+        }
+    }
+
+    // Transaction
+    public void SaveTransactionData(Transaction transaction)
+    {
+        File.AppendAllText(filePath, transaction.ToString());
     }
 }
-
