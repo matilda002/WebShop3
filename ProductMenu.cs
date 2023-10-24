8230bdffce69d8099bbebdf7f220b9e578a449a2
matilda002 <matilda0817@hotmail.com>
2023-10-24 11:58:44 +0200

    re-made menu

--- ProductMenu.cs
+++ ProductMenu.cs
@@ -1,63 +1,46 @@
 ﻿namespace WebShop3;
 
-public class ProductMenu : EditProduct
+public class ProductMenu : Admin
 {
     public void StockMenu()
     {
-        string? forward = string.Empty;
-        do // doing a loop since I don't want to return to the main menu until there's a valid answer
-        {
-            Console.Clear();
-            Console.WriteLine("--------- Edit Products ---------");
-            // asking if the user wants to go foward and change any products
-            Console.Write("Do you want to change the range of products? [y/n]:  ");
-            forward = Console.ReadLine()?.ToLower();
-
-            switch (forward)
-            {
-                case "y":
-                    StockMenu2();
-                    break;
-                case "n":
-                    Console.WriteLine("You have chosen to quit!");
-                    break;
-                default:
-                    Console.WriteLine("Unvalid answer, try again by pressing the ENTER key!");
-                    Console.ReadKey();
-                    break;
-            }
-        }
-        while (forward != "n");
-    }
-
-    public void StockMenu2()
-    {
-        string? forward2 = string.Empty;
+        bool forward = false;
         do
         {
             Console.Clear();
-            Console.WriteLine("--------- Edit Products ---------");
-            // asking if the user wants to add or remove items
-            Console.Write("Do you want to add or remove product? [a/r]\nTo quit answer 'q':  ");
-            forward2 = Console.ReadLine()?.ToLower();
+            Console.WriteLine("--------- Product Overview ---------\n" +
+                              "1. Create a new product\n" +
+                              "2. Print out list of products\n" +
+                              "3. Update a product\n" +
+                              "4. Delete a product\n\n" +
+                              "5. Quit");
+            Console.Write("\nWrite your menuchoice:  ");
+            int.TryParse(Console.ReadLine(), out int successful);
 
-            switch (forward2)
+            switch (successful)
             {
-                case "a":
-                    AddItems();
-                    break;
-                case "r":
-                    RemoveItems();
-                    break;
-                case "q":
-                    Console.WriteLine("You have chosen to quit!"); // go back to StockMenu() to quit program
-                    break;
+                case 1:
+                    CreateProduct();
+                    forward = true; break;
+                case 2:
+                    ReadProduct();
+                    forward = true; break;
+                case 3:
+                    UpdateProduct();
+                    forward = true; break;
+                case 4:
+                    DeleteProduct();
+                    forward = true; break;
+                case 5:
+                    Console.WriteLine("You have chosen to quit!\n\nPress 'ENTER' to continue!"); // quit program
+                    Console.ReadKey();
+                    return;
                 default:
-                    Console.WriteLine("Unvalid answer, try again by pressing the ENTER key!");
+                    Console.WriteLine("Not a valid menuchoice!\n\nPress 'ENTER' to continue!");
                     Console.ReadKey();
                     break;
             }
         }
-        while (forward2 != "q");
+        while (!forward);
     }
 }
