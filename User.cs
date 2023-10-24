bbf38b833ce9bce1e7842b843858fb092784513a
matilda002 <matilda0817@hotmail.com>
2023-10-24 13:31:18 +0200

    resolve merging conflicts

--- User.cs
+++ User.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
-
-namespace WebShop3;
+﻿namespace WebShop3;
 
 public class User
 {
     public string UserName { get; set; }
     public string PassWord { get; set; }
-<<<<<<< HEAD
     public UserRole Role { get; set; }
 
 
@@ -23,37 +13,18 @@ public class User
 
     public User(string userName, string passWord, UserRole role = UserRole.User)
     {
-=======
->>>>>>> 3d18adf435594f857c54b349b6829be9f5b1280f
 
-    private string filePath;
-
-    List<Product> _boughtProducts = new List<Product>();
-
-    public User(string userName, string passWord)
-    {
         UserName = userName;
         PassWord = passWord;
-<<<<<<< HEAD
         Role = role;
         Transaction _transaction = new Transaction(_boughtProducts);
         filePath = $"../../../transactions/transaction_{UserName}";
 
-=======
-
-        Transaction _transaction = new Transaction(_boughtProducts);
-        filePath = $"../../../transactions/transaction_{UserName}";
-
->>>>>>> 3d18adf435594f857c54b349b6829be9f5b1280f
         SaveTransactionData(_transaction);
         DisplayTransactionData();
     }
 
-<<<<<<< HEAD
     void DisplayTransactionData()
-=======
-    public void DisplayTransactionData()
->>>>>>> 3d18adf435594f857c54b349b6829be9f5b1280f
     {
         string[] contentOfFile = File.ReadAllLines(filePath);
 
@@ -69,11 +40,7 @@ public class User
     }
 
     // Transaction
-<<<<<<< HEAD
     void SaveTransactionData(Transaction transaction)
-=======
-    public void SaveTransactionData(Transaction transaction)
->>>>>>> 3d18adf435594f857c54b349b6829be9f5b1280f
     {
         File.AppendAllText(filePath, transaction.ToString());
     }
