d20ac1fa0081a46df1bd56b8a49563fbd0d1a095
matilda002 <matilda0817@hotmail.com>
2023-10-24 13:22:15 +0200

    added Elias code for transaction

--- User.cs
+++ User.cs
@@ -5,13 +5,44 @@ public class User
     public string UserName { get; set; }
     public string PassWord { get; set; }
     public UserRole Role { get; set; }
+
+
+    private string filePath;
+
+    List<Product> _boughtProducts = new List<Product>();
+
     public User(string userName, string passWord, UserRole role = UserRole.User)
     {
 
         UserName = userName;
         PassWord = passWord;
         Role = role;
+        Transaction _transaction = new Transaction(_boughtProducts);
+        filePath = $"../../../transactions/transaction_{UserName}";
 
+        SaveTransactionData(_transaction);
+        DisplayTransactionData();
+    }
+
+    void DisplayTransactionData()
+    {
+        string[] contentOfFile = File.ReadAllLines(filePath);
+
+        foreach (string line in contentOfFile)
+        {
+            string[] info = line.Split(", ");
+
+            Product product = new Product(contentOfFile[0], int.Parse(contentOfFile[1]));
+            _boughtProducts.Add(product);
+            Transaction transaction = new Transaction(_boughtProducts);
+            Console.WriteLine(transaction.ToString());
+        }
+    }
+
+    // Transaction
+    void SaveTransactionData(Transaction transaction)
+    {
+        File.AppendAllText(filePath, transaction.ToString());
     }
 }
 
